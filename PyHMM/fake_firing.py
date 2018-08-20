6da32be0ce3943a93902050145cfad5d79f0e1e3
abuzarmahmood <abuzarmahmood@gmail.com>
2018-08-20 13:55:31 -0400

    Code for categorical fake data

--- PyHMM/fake_firing.py
+++ PyHMM/fake_firing.py
@@ -9,28 +9,7 @@
 # Import stuff
 import numpy as np
 import pylab as plt
-
-###############
-# CATEGORICAL #
-###############
-def fake_cat_firing(nrns,trials,length,num_states,state_order,ceil_p,jitter_t,min_duration):
-    # nrns = number of neurons (emissions)
-    # trials = number of trials
-    # length = number of bins for data
-    # state_order = LIST: fixed order for states
-    # ceil_p = maximum firing probability
-    # jitter_t = max amount of jitter between neurons for a state transition
-    # min_duration =    time between start and first transition
-    #                   time between final state and end
-    #                   time between intermediate state transitions
     
-    
-    # Returns data array, transition times, emission probabilities
-    data = np.zeros((nrns, trials, length)) # neurons x trials x time
-    
-    # Emission probabilities of neurons for every state
-    p = np.random.rand(num_states, data.shape[0])*ceil_p # states x neuron
-      
 #############
 # BERNOULLI #
 #############                                       
@@ -92,6 +71,44 @@ def fake_ber_firing(nrns,trials,length,num_states,state_order,ceil_p,jitter_t,mi
                         data[neuron, trial, time] = np.random.binomial(1, p[state_order[trans_count],neuron])
     return data, t, p
 
+###############
+# CATEGORICAL #
+###############
+# Approximation to categorical data since I'm lazy
+# Take data from bernoulli trials and convert to categorical
+def fake_cat_firing(nrns,trials,length,num_states,state_order,ceil_p,jitter_t,min_duration):
+    # nrns = number of neurons (emissions)
+    # trials = number of trials
+    # length = number of bins for data
+    # state_order = LIST: fixed order for states
+    # ceil_p = maximum firing probability
+    # jitter_t = max amount of jitter between neurons for a state transition
+    # min_duration =    time between start and first transition
+    #                   time between final state and end
+    #                   time between intermediate state transitions
+    
+    
+    # Returns data array, transition times, emission probabilities
+    ber_data = fake_ber_firing(nrns,trials,length,num_states,state_order,ceil_p,jitter_t,min_duration)
+    ber_data, t, p = ber_data[0], ber_data[1], ber_data[2]
+    
+    # Remove multiple spikes in same time bin (for categorical HMM)
+    for i in range(ber_data.shape[0]): # Loop over trials
+        for k in range(ber_data.shape[2]): # Loop over time
+            n_firing_units = np.where(ber_data[i,:,k] > 0)[0]
+            if len(n_firing_units)>0:
+                ber_data[i,:,k] = 0
+                ber_data[i,np.random.choice(n_firing_units),k] = 1
+    
+    # Convert bernoulli trials to categorical data        
+    cat_binned_spikes = np.zeros((ber_data.shape[0],ber_data.shape[2]))
+    for i in range(cat_binned_spikes.shape[0]):
+        for j in range(cat_binned_spikes.shape[1]):
+            firing_unit = np.where(ber_data[i,:,j] > 0)[0]
+            if firing_unit.size > 0:
+                cat_binned_spikes[i,j] = firing_unit + 1
+    
+    return ber_data, t, p
 
 # Raster plot
 def raster(data,trans_times=None,expected_latent_state=None):
@@ -104,10 +121,7 @@ def raster(data,trans_times=None,expected_latent_state=None):
     # Red lines indicate mean transition times
     # Yellow ticks indicate individual neuron transition times
     
-    # Plot state probability
-    if expected_latent_state is not None:
-        plt.plot(expected_latent_state.T*data.shape[0])
-        
+       
     # Plot individual neuron transition times
     if trans_times is not None:
         for unit in range(data.shape[0]):
@@ -120,10 +134,16 @@ def raster(data,trans_times=None,expected_latent_state=None):
             for time in range(data.shape[1]):
                 if data[unit, time] > 0:
                     plt.vlines(time, unit, unit + 0.5, linewidth = 0.5)
+        # Plot state probability
+        if expected_latent_state is not None:
+            plt.plot(expected_latent_state.T*data.shape[0])
+        
     else:
        for time in range(data.shape[0]):
            if data[time] > 0:
-               plt.vlines(time, data[time], data[time] + 0.5, linewidth = 0.5) 
+               plt.vlines(time, data[time], data[time] + 0.5, linewidth = 0.5)
+       if expected_latent_state is not None:
+            plt.plot(expected_latent_state.T*np.unique(data).size)
     
     # Plot mean transition times         
     if trans_times is not None:
